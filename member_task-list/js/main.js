04dffbef003375493b8e123de85973ec6ccf9d40
Amiya <2474839653@qq.com>
2022-12-28 23:07:17 +0800

    新增成员Ljy

--- member_task-list/js/main.js
+++ member_task-list/js/main.js
@@ -1,5 +1,6 @@
-function getUrl(type){
-  var url = 'https://zyzx-s.zy.com/api/1/aipt/get_like_tasks?pageNo=1&pageSize=10&access_token=6ff9dd8c-dcbf-414f-8a02-6c693de20b7e'
+function getUrl(type) {
+  var url =
+    "https://zyzx-s.zy.com/api/1/aipt/get_like_tasks?pageNo=1&pageSize=10&access_token=6ff9dd8c-dcbf-414f-8a02-6c693de20b7e";
 }
 
 function getExamUrl(
@@ -7,70 +8,92 @@ function getExamUrl(
   pageSize = 10,
   finished = false,
   access_token = ""
-){
+) {
   var url = `https://zyzx-s.zy.com/api/1/aipt/get_tasks?\
 pageNo=${pageNo}\
 &pageSize=${pageSize}\
 &finished=${finished}\
 &access_token=${access_token}`;
-return url
+  return url;
 }
-async function getJson(
-url
-) {
+async function getJson(url) {
+  let data = await axios
+    .get(url)
+    .then(function (result) {
+      var json_str = JSON.stringify(result.data);
 
+      if (json_str) {
+        var json = JSON.parse(json_str);
+      }
+      // console.log(json);
+      return json;
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+  return data;
+}
 
-let data = await axios.get(url)
-.then(function (result) {
-    var json_str = JSON.stringify(result.data);
-    
-    if (json_str) {
-      var json = JSON.parse(json_str);
-    }
-    // console.log(json);
-    return json;
+function addTask(json, id = "Chinese", a) {
+  for (var i = 0; i < json.data.list.length; i++) {
+    var point = "&nbsp;&bull;&nbsp;";
+    var str =
+      `<span class="badge bg-warning">${json.data.list[i]["subject_name"]}</span> ` +
+      "</br>" +
+      point +
+      json.data.list[i].task_name +
+      "<br>&nbsp;&bull;&nbsp;发布日期: " +
+      json.data.list[i]["publish_time"] +
+      "<br>&nbsp;&bull;&nbsp;截止日期: " +
+      json.data.list[i].deadline +
+      "<br>" +
+      // '&nbsp;&bull;&nbsp;教师备注: ' +
+      // json.data.list[i].remarks + "<br>"+
+      `<span class="badge bg-secondary">${json.data.list[i]["res_type_name"]}</span> ` +
+      `<span class="badge bg-secondary">${json.data.list[i]["exercise_type_name"]}</span> ` +
+      // `<button type="button" class="btn btn-primary btn-sm" @click="likeTask('${json.data.list[i]["task_id"]}')">收藏</button>`
+      "<br>";
+    var exam_url =
+      json.data.list[i]["resource_list"][0]["resource_preview_url"];
+
+    var task = document.createElement("li");
+    // mb-3
+    task.className =
+      "list-group list-group-item list-group-item-action list-group-item-success";
+    task.setAttribute("onclick", `location.href='${exam_url}'`);
+    task.setAttribute("style", "cursor:pointer");
+    // task.href = exam_url;
+    task.innerHTML = str;
+    var div = document.getElementById(id);
+    //         console.log(json.data.list[i]['subject_name']);
+
+    var card_body = div.getElementsByClassName("card-body");
+    card_body[0].appendChild(task);
+
+    // 不同作业末尾换行
+    var brDiv = document.createElement("br");
+    card_body[0].appendChild(brDiv);
+  }
+}
+
+async function likeTask(access_token, taskId, like = true) {
+  var url = `https://zyzx-s.zy.com/api/1/aipt/like_task?access_token=${access_token}`;
+  var bodyFormData = {
+    taskId: taskId,
+    like: like,
+  };
+  axios({
+    method: "post",
+    url: url,
+    data: bodyFormData,
+    headers: { "Content-Type": "multipart/form-data" },
   })
-  .catch(function (error) {
-    console.log(error);
-  });
-return data;
-};
-
-function addTask(json,id='Chinese', a){
-    for (var i = 0; i < json.data.list.length; i++) {
-        var point = '&nbsp;&bull;&nbsp;'
-        var str = `<span class="badge bg-warning">${json.data.list[i]['subject_name']}</span> ` +
-         '</br>' +point+
-          json.data.list[i].task_name +
-           '<br>&nbsp;&bull;&nbsp;发布日期: ' +
-            json.data.list[i]['publish_time'] +
-             '<br>&nbsp;&bull;&nbsp;截止日期: ' +
-              json.data.list[i].deadline + "<br>"+
-              // '&nbsp;&bull;&nbsp;教师备注: ' +
-              // json.data.list[i].remarks + "<br>"+
-              `<span class="badge bg-secondary">${json.data.list[i]['res_type_name']}</span> `+
-              `<span class="badge bg-secondary">${json.data.list[i]['exercise_type_name']}</span>`+
-              "<br>";
-        var exam_url = json.data.list[i]['resource_list'][0]['resource_preview_url'];
-
-
-        var task = document.createElement("li");
-        // mb-3
-        task.className = "list-group list-group-item list-group-item-action list-group-item-success";
-        task.setAttribute('onclick', `location.href='${exam_url}'`);
-        task.setAttribute('style', "cursor:pointer");
-        // task.href = exam_url;
-        task.innerHTML = str;
-        var div = document.getElementById(id);
-        //         console.log(json.data.list[i]['subject_name']);
-
-        var card_body = div.getElementsByClassName('card-body');
-        card_body[0].appendChild(task);
-
-        // 不同作业末尾换行
-        var brDiv = document.createElement('br');
-        card_body[0].appendChild(brDiv);
-
-
-    }
+    .then(function (response) {
+      //handle success
+      console.log(response);
+    })
+    .catch(function (response) {
+      //handle error
+      console.log(response);
+    });
 }
